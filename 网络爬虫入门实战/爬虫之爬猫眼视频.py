fa51fac69149d87a641eb18bd0ebade406563890
1911080146chenjiongyu <1156341440@qq.com>
2021-12-28 13:02:32 +0800

    add 爬虫 爬视频 爬照片

--- 网络爬虫入门实战/爬虫之爬猫眼视频.py
+++ 网络爬虫入门实战/爬虫之爬猫眼视频.py
@@ -1,7 +1,8 @@
 import requests
 from lxml import etree
 import time
-
+import re
+import os
 
 # 获取网页源代码
 def get_page(url):
@@ -26,37 +27,50 @@ def parse_page1(html):
     etree.HTML()：
     调用HTML类对HTML文本进行初始化，成功构造XPath解析对象，同时可以自动修正HTML文本（标签缺少闭合自动添加上）
     '''
-    pages = html_elem.xpath('//*[@id="app"]/div/div/div[1]/dl/dd/div/div')
     '''
     快速获取xpath：
     先F12,然后用Ctrl+Shift+C快速选中Web界面的元素，然后在元素上右键-->复制-->复制XPath
     '''
-    for i in pages:
-        titles = i.xpath('./div[1]/p[1]/a/text()')
-        infos = i.xpath('./div[1]/p[2]/text()')
-        times = i.xpath('./div[1]/p[3]/text()')
-        scores = i.xpath('./div[2]/p/i/text()')
-        '''
-        text()函数
-        获取元素文本内容
-        '''
-        print("----------------------------------------")
-        print("电影名：", titles[0], '\n', "", infos[0].strip(), '\n', "", times[0].strip(), '\n', "",
-              "".join(scores) + '分')
+    movies_urls = html_elem.xpath('//h4[@class="video-name one-line"]/a[@href]/@href')
+    movies_names = html_elem.xpath('//h4[@class="video-name one-line"]/a[@href]/text()')
+    '''
+    text()函数
+    获取元素文本内容
+    '''
+    print("----------------------------------------")
+    for movies_url, movies_name in zip(movies_urls, movies_names):
+        print(movies_url, movies_name)
 
-        time.sleep(1)
+        # 获取文本
+        movie_id_string = requests.get(movies_url).text
+        print(movie_id_string)
+
+        # . 表示所有的,但是只匹配一个,除了/n
+        # * 表示按照前面的方式进行匹配
+        movie_mp4_url = re.search('source src="(.*)" type', movie_id_string).group(1)
+        print(movie_mp4_url)
+
+        # 因为视频是二进制文件,所以需要获取content
+        mp4 = requests.get(movie_mp4_url).content
+
+        # 写入文件夹
+        with open('./movie/%s.mp4' % movies_name, 'wb') as data_file:
+            data_file.write(mp4)
+
+    time.sleep(1)
 
 
 # 开始爬取网页
 def crawl():
-    url = 'https://maoyan.com/board/4?offset={0}'
+    url = 'https://www.maoyan.com/news?showTab=3&showTab=3&offset={0}'
     '''
     用Request时对于大部分的页面来说只需要两个参数，一个是url也就是请求的网址，另外一个就是headers，
     而headers中最重要的就是user-agent，其他的参数都没有这两个参数重要。
     所以可以只留下这两个参数再去请求页面，如果请求不下来再去尝试添加其他参数。
     '''
     print('开始爬取')
-    for page in range(0, 110, 10):  # 0 25 50 75
+    os.makedirs("movie")
+    for page in range(0, 160, 16):  # 0 25 50 75
         # print('正在爬取第 ' + str(page+1) + ' 页至第 ' + str(page+25) + ' 页......')
         html = get_page(url.format(page))
         data = parse_page1(html)
--- 网络爬虫入门实战/爬虫之爬豆瓣照片.py
+++ 网络爬虫入门实战/爬虫之爬豆瓣照片.py
@@ -1,6 +1,7 @@
 import requests
 from lxml import etree
 import time
+import os
 
 
 # 获取网页源代码
@@ -20,46 +21,52 @@ def get_page(url):
 
 
 # 解析网页源代码
-def parse_page1(html):
+def parse_page1(html, page):
     html_elem = etree.HTML(html)
+    result = etree.tostring(html_elem)  # 输出修正后的html
     '''
     etree.HTML()：
     调用HTML类对HTML文本进行初始化，成功构造XPath解析对象，同时可以自动修正HTML文本（标签缺少闭合自动添加上）
     '''
-    pages = html_elem.xpath('//*[@id="app"]/div/div/div[1]/dl/dd/div/div')
+    # pages = html_elem.xpath('//*[@id="app"]/div/div/div[1]/dl/dd/div/div')
     '''
     快速获取xpath：
     先F12,然后用Ctrl+Shift+C快速选中Web界面的元素，然后在元素上右键-->复制-->复制XPath
     '''
-    for i in pages:
-        titles = i.xpath('./div[1]/p[1]/a/text()')
-        infos = i.xpath('./div[1]/p[2]/text()')
-        times = i.xpath('./div[1]/p[3]/text()')
-        scores = i.xpath('./div[2]/p/i/text()')
-        '''
-        text()函数
-        获取元素文本内容
-        '''
-        print("----------------------------------------")
-        print("电影名：", titles[0], '\n', "", infos[0].strip(), '\n', "", times[0].strip(), '\n', "",
-              "".join(scores) + '分')
+    address = html_elem.xpath('//div[@class="pl2"]/a/@href')
+    title = html_elem.xpath('//div[@class="pl2"]/a/@title')
+    info = html_elem.xpath('//p[@class="pl"]/text()')
+    num = html_elem.xpath('//div[@class="star clearfix"]/span[@class="rating_nums"]/text()')
+    pic = html_elem.xpath('//a[@class="nbg"]/img/@src')
+    '''
+    text()函数
+    获取元素文本内容
+    '''
+    os.makedirs("books")
+    for j in range(0, 25):
+        print(pic[j])
+        with open('books\\' + str((page * 25) + j + 1) + '.jpg', 'wb') as fp:
+            pi = requests.get(pic[j])
+            fp.write(pi.content)
+            print("保存第%d本书成功" % int((page * 25) + j + 1))
 
-        time.sleep(1)
+    time.sleep(1)  # 延迟1秒
 
 
 # 开始爬取网页
 def crawl():
-    url = 'https://maoyan.com/board/4?offset={0}'
+    url = 'https://book.douban.com/top250?start={0}'
     '''
     用Request时对于大部分的页面来说只需要两个参数，一个是url也就是请求的网址，另外一个就是headers，
     而headers中最重要的就是user-agent，其他的参数都没有这两个参数重要。
     所以可以只留下这两个参数再去请求页面，如果请求不下来再去尝试添加其他参数。
     '''
     print('开始爬取')
-    for page in range(0, 110, 10):  # 0 25 50 75
+    for page in range(0, 110, 10):  # 0 25 50 7 5
         # print('正在爬取第 ' + str(page+1) + ' 页至第 ' + str(page+25) + ' 页......')
         html = get_page(url.format(page))
-        data = parse_page1(html)
+
+        data = parse_page1(html, page)
         time.sleep(1)  # 延迟1秒
     print('结束爬取')
 
